6e02768cbe74cbf2120ef9317640aa69a31c428d
Diya Jain <diya.jain@vyaparapp.in>
2023-05-01 20:03:02 +0530

    hkjkh

--- calculator.py
+++ calculator.py
@@ -1,9 +1,12 @@
 from .sum import *
 from .sub import *
+from .multiply import *
 
 def calculation(x,y):
     addition = add(x,y)
     print(addition)
     subtraction = sub(x,y)
     print(subtraction)
+    multiplication = multi(x,y)
+    print(multiplication)
 
