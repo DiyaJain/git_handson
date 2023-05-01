a057caa6a439d47fcb13fe37438e3e20df7331e9
Diya Jain <diya.jain@vyaparapp.in>
2023-05-01 20:04:56 +0530

    Revert "hkjkh"
    This reverts commit 6e02768cbe74cbf2120ef9317640aa69a31c428d.

--- calculator.py
+++ calculator.py
@@ -1,12 +1,9 @@
 from .sum import *
 from .sub import *
-from .multiply import *
 
 def calculation(x,y):
     addition = add(x,y)
     print(addition)
     subtraction = sub(x,y)
     print(subtraction)
-    multiplication = multi(x,y)
-    print(multiplication)
 
